b3555c0453312ec63928003c664208c33dff2ade
RozimuhammadIsomiddinov <159604873+RozimuhammadIsomiddinov@users.noreply.github.com>
2024-08-27 11:39:09 +0500

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -10,7 +10,14 @@ connectToDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors(
+  {
+  origin:["https://great-idea-frontend.vercel.app"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+  }
+  
+));
 
 app.use("/", app1);
 app.listen(process.env.PORT, () => {
